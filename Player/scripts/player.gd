b68cc9125813fa5ffe5cc7fa1dddda3dc3b1ca6f
JuantMartinez17 <juantomasm0@gmail.com>
2024-12-16 00:06:38 -0300

    run, jump and fall animations implemented

--- Player/scripts/player.gd
+++ Player/scripts/player.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+@onready var ap = $AnimationPlayer
+@onready var sprite = $Sprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -21,5 +22,20 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if (direction != 0):
+		sprite.flip_h = (direction == -1)
 
 	move_and_slide()
+	update_animation(direction)
+
+func update_animation(direction):
+	if is_on_floor():
+		if direction == 0:
+			ap.play("idle")
+		else:
+			ap.play("run")
+	else:
+		if velocity.y < 0:
+			ap.play("jump")
+		elif velocity.y > 0:
+			ap.play("fall")
