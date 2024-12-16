6a2a55310109f633bfe56a2b5cb353c6968f7165
JuantMartinez17 <juantomasm0@gmail.com>
2024-12-16 00:58:58 -0300

    Crouch idle, walk and attack animations added. [TODO] script not working correctly when crouched

--- Player/scripts/player.gd
+++ Player/scripts/player.gd
@@ -3,9 +3,11 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
+const CROUCH_SPEED = 100.0
 @onready var ap = $AnimationPlayer
 @onready var sprite = $Sprite2D
 var is_attacking = false #avoid interrumtions in attack animations and stop movement
+var is_crouched = false #flag to determine if player is crouching
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -15,12 +17,25 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	#handle player crouching
+	if Input.is_action_pressed("crouch"):
+		is_crouched = true
+		ap.play("crouch")
+	if Input.is_action_just_released("crouch"):
+		is_crouched = false
 		
 	#handle attack animations
 	if Input.is_action_just_pressed("attack"):
-		play_attack_animation("attack")
+		if is_crouched:
+			play_attack_animation("crouch_attack")
+		else:
+			play_attack_animation("attack")
 	elif Input.is_action_just_pressed("attack 2"):
-		play_attack_animation("attack")
+		if is_crouched:
+			play_attack_animation("crouch_attack")
+		else: 
+			play_attack_animation("attack")
 		
 	#stop movement while attacking
 	if is_attacking :
@@ -32,7 +47,10 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		if is_crouched:
+			velocity.x = direction * CROUCH_SPEED
+		else:
+			velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	if (direction != 0):
@@ -43,10 +61,16 @@ func _physics_process(delta: float) -> void:
 
 func update_animation(direction):
 	if is_on_floor():
-		if direction == 0:
-			ap.play("idle")
+		if is_crouched:
+			if direction == 0:
+				ap.play("crouch")
+			else:
+				ap.play("crouch_walk")
 		else:
-			ap.play("run")
+			if direction == 0:
+				ap.play("idle")
+			else:
+				ap.play("run")
 	else:
 		if velocity.y < 0:
 			ap.play("jump")
