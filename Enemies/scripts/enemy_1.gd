1f675f869715c2b4b06e26a6a99f3d7b9457f879
JuantMartinez17 <juantomasm0@gmail.com>
2024-12-16 17:29:28 -0300

    Enemy chases player when player is at a certain distance

--- Enemies/scripts/enemy_1.gd
+++ Enemies/scripts/enemy_1.gd
@@ -1,27 +1,36 @@
 extends CharacterBody2D
 
-
-const SPEED = 180.0
+const SPEED = 120.0
 const JUMP_VELOCITY = -400.0
 @export var max_health: int = 50
 @export var damage: int = 15
 @export var speed: float = SPEED
+@export var detection_range: float = 600.0 # Distancia para comenzar a moverse hacia el jugador
 @onready var ap = $AnimationPlayer
-@onready var sprite = $Sprite
+@onready var sprite = $Sprite2D
+@onready var detection_area = $DetectionArea2D
+@onready var attack_area = $AttackArea2D
 
 var current_health: int = max_health
 var is_dead: bool = false
+var is_chasing: bool = false
 var is_attacking: bool = false
-
+var player: CharacterBody2D = null
 
 func _physics_process(delta: float) -> void:
 	if is_dead: 
 		return
-	
-	velocity.x = 0
+
+	if is_chasing and not is_attacking:
+		var direction_to_player = (player.global_position - global_position).normalized()
+		sprite.flip_h = direction_to_player.x < 0
+		velocity.x = direction_to_player.x * SPEED
+		ap.play("run")
+	else:
+		velocity.x = 0
 	move_and_slide()
-	
-func take_damage(amount: int)-> void:
+
+func take_damage(amount: int) -> void:
 	if is_dead:
 		return
 	current_health -= amount
@@ -31,17 +40,17 @@ func take_damage(amount: int)-> void:
 	else:
 		on_damage_taken()
 
-func on_damage_taken()-> void:
+func on_damage_taken() -> void:
 	ap.play("hit")
-	
-func die()-> void:
+
+func die() -> void:
 	is_dead = true
 	velocity = Vector2.ZERO
 	ap.play("death")
 	await ap.animation_finished
 	queue_free()
-	
-func attack(player)-> void:
+
+func attack(player) -> void:
 	if is_attacking or is_dead:
 		return
 	is_attacking = true
@@ -50,8 +59,24 @@ func attack(player)-> void:
 	is_attacking = false
 	player.take_damage(damage)
 
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_detection_area_2d_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		print("Jugador detectado. atacando...")
-		attack(body)
+		player = body
+		is_chasing = true
+
+
+func _on_detection_area_2d_body_exited(body: Node2D) -> void:
+	if body == player:
+		player = null
+		is_chasing = false
+
+
+func _on_attack_area_2d_body_entered(body: Node2D) -> void:
+	if body == player:
+		is_chasing = false
+		attack(player)
+
+
+func _on_attack_area_2d_body_exited(body: Node2D) -> void:
+	if body == player:
+		is_chasing = true
