590e48cfd0a52e161f73e7b9df37976b6f347d0b
JuantMartinez17 <juantomasm0@gmail.com>
2024-12-17 14:18:54 -0300

    player can travel to figth final boss

---
+++ Objects/shop/scripts/level_boss_trip_a.gd
@@ -0,0 +1,40 @@
+extends Area2D
+
+@onready var destination = get_tree().root.get_node("res://Objects/shop/scenes/level_boss_trip_b.tscn")
+@onready var hud = get_tree().root.get_node("res://UI/scenes/hud.tscn")
+
+var player_in_area = false
+var player = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event):
+	if player_in_area and event.is_action_pressed("travel"):
+		teleport_player()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		player_in_area = true
+		player = body
+		hud.show_message("Press E to fight final boss")
+		print("Presiona enter para viajar")
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		player_in_area = false
+		player = null
+		hud.hide_message()
+
+func teleport_player():
+	if destination and player:
+		player.global_position = destination.global_position
+		hud.hide_message()
+		print("Teletransportado a: ", destination.name)
--- UI/scripts/hud.gd
+++ UI/scripts/hud.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 @onready var health_bar = $HealthBar
+@onready var message_label = $Label
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,3 +14,11 @@ func _process(delta: float) -> void:
 func update_health(current_healt: int, max_health: int)-> void:
 	health_bar.value = current_healt
 	health_bar.max_value = max_health
+	
+func show_message(text: String):
+	message_label.text = text
+	message_label.visible = true
+
+func hide_message()-> void:
+	message_label.visible = false
+	
