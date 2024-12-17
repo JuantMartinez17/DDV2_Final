7211d7bea31adc6ec68451152cd2f7f07fd513d1
JuantMartinez17 <juantomasm0@gmail.com>
2024-12-17 11:22:56 -0300

    load key bindings file at init

--- UI/scripts/mainmenu.gd
+++ UI/scripts/mainmenu.gd
@@ -3,7 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	load_key_config()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,3 +20,27 @@ func _on_exit_pressed() -> void:
 	
 func _on_controls_pressed() -> void:
 	get_tree().change_scene_to_file("res://UI/scenes/controls_config.tscn")
+	
+func load_key_config()-> void:
+	var file_path = "res://input_config.cfg"
+	if FileAccess.file_exists(file_path):
+		var file = FileAccess.open(file_path, FileAccess.READ)
+		if file:
+			while not file.eof_reached():
+				var line = file.get_line()
+				if line == "":
+					continue
+				var parts = line.split("=")
+				if parts.size() == 2:
+					var action = parts[0]
+					var key_string = parts [1]
+					var event = InputEventKey.new()
+					event.keycode = OS.find_keycode_from_string(key_string)
+					InputMap.action_erase_events(action)
+					InputMap.action_add_event(action, event)
+			file.close()
+			print("key bindings loaded")
+		else:
+			print("File not found")
+					
+				
